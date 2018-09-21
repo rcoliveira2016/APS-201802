c94bb42b29a88c91047c7870ac01f5614e1104c1
cubacubista <trabalhofsgbabok@gmail.com>
2018-09-20 22:33:18 -0300

    mask

---
+++ src/APS.Domain/Models/Agendamentos/Agendamento.cs
@@ -0,0 +1,22 @@
+﻿using APS.Domain.Models.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS.Domain.Models.Agendamentos
+{
+    public class Agendamento
+    {
+
+        public Cliente Cliente { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime HoraInicial { get; set; }
+        public DateTime HoraFinal { get; set; }
+        public string Endereço { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+
+    }
+}
