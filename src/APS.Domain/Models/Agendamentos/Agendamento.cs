4ebbcb691413538e19bacd9d1f2d04e58b5ef05e
cubistas <trabalhofsgbabok@gmail.com>
2018-09-21 17:14:11 -0300

    agendamentos

--- src/APS.Domain/Models/Agendamentos/Agendamento.cs
+++ src/APS.Domain/Models/Agendamentos/Agendamento.cs
@@ -1,4 +1,5 @@
-﻿using APS.Domain.Models.Clientes;
+﻿using APS.Domain.Core.Models.Common;
+using APS.Domain.Models.Clientes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +8,14 @@ using System.Threading.Tasks;
 
 namespace APS.Domain.Models.Agendamentos
 {
-    public class Agendamento
+    public class Agendamento:Entity
     {
 
         public Cliente Cliente { get; set; }
         public DateTime Data { get; set; }
         public DateTime HoraInicial { get; set; }
         public DateTime HoraFinal { get; set; }
-        public string Endereço { get; set; }
+        public string Endereco { get; set; }
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
 
---
+++ src/APS.Domain/Service/AgendamentoService.cs
@@ -0,0 +1,51 @@
+﻿using APS.Domain.Interfaces.Service;
+using APS.Domain.Core.Service;
+using APS.Domain.Models.Usurios;
+using System.Linq;
+using APS.Domain.Interfaces.Repository.Usuario;
+using APS.Domain.Core.Interface;
+using APS.Domain.Core.Exceptions;
+using APS.Domain.Models.Clientes;
+using APS.Domain.Models.Agendamentos;
+using System;
+
+namespace APS.Domain.Service
+{
+    public class AgendamentoService : ServiceCRUD<Agendamento>, IAgendamentoService
+    {
+        
+
+        public AgendamentoService(IAgendamentoRepository agendamentoRepository, IUnitOfWork unitOfWork) :base(unitOfWork, agendamentoRepository)
+        {
+            
+        }
+
+        protected override void ValidarCadastro(Agendamento entidade)
+        {           
+            this.ValidacaoBasica(entidade);
+
+            ValidarRegras().IsValid();
+        }
+
+        protected override void ValidarAtualizar(Agendamento entidade)
+        {
+
+            this.ValidacaoBasica(entidade);
+
+            ValidarRegras().IsValid();
+        }        
+
+
+        private void ValidacaoBasica(Agendamento entidade) {
+            ValidarRegras(entidade)
+                .NotEmpty(x => x.Descricao, "Descrição vazia")
+                .NotEmpty(x => x.Endereco, "Endereço vazio")
+                .Greater(x => x.Data, DateTime.Now, "Data não pode ser inferior a data atual");
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+    }
+}
